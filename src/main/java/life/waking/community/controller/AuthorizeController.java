a1c69848dfdd1e2624645a064cd016ee6109d216
waking123 <wenwang@wondersgroup.com>
2019-09-05 21:02:17 +0800

    获取到token

--- src/main/java/life/waking/community/controller/AuthorizeController.java
+++ src/main/java/life/waking/community/controller/AuthorizeController.java
@@ -1,6 +1,7 @@
 package life.waking.community.controller;
 
 import life.waking.community.dto.AccessTokenDTO;
+import life.waking.community.dto.GithubUser;
 import life.waking.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,13 +18,14 @@ public class AuthorizeController {
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        githubProvider.getAccessToken(accessTokenDTO);
-        accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887//callback");
-        accessTokenDTO.setState(state);
         accessTokenDTO.setClient_id("7e1865bf3b383ce7e1b1");
         accessTokenDTO.setClient_secret("74143192fef6494d122f8db64d470b20943b7969");
-
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user);
         return "index";
     }
 }
--- src/main/java/life/waking/community/provider/GithubProvider.java
+++ src/main/java/life/waking/community/provider/GithubProvider.java
@@ -11,27 +11,28 @@ import java.io.IOException;
 
 @Component
 public class GithubProvider {
-    public String getAccessToken(AccessTokenDTO accessTokenDTO){
-        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
-        RequestBody body = RequestBody.create(JSON, json);
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
         Request request = new Request.Builder()
-                    .url("https://github.com/login/oauth/access_token")
-                    .post(body)
-                    .build();
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
         try (Response response = client.newCall(request).execute()) {
             String string = response.body().string();
-            System.out.println(string);
-            return string;
-        } catch (IOException e) {
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return  null;
+        return null;
     }
 
-    public GithubUser getUser(String accessToken){
+    public GithubUser getUser(String accessToken) {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("https://api.github.com/user?access_token="+accessToken)
+                .url("https://api.github.com/user?access_token=" + accessToken)
                 .build();
         try {
             Response response = client.newCall(request).execute();
