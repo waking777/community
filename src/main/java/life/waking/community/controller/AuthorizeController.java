92e35f5b930d9a439c968de08e5538d45b851b99
waking123 <wenwang@wondersgroup.com>
2019-09-05 22:05:53 +0800

    用配置文件来配置变量

--- src/main/java/life/waking/community/controller/AuthorizeController.java
+++ src/main/java/life/waking/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.waking.community.dto.AccessTokenDTO;
 import life.waking.community.dto.GithubUser;
 import life.waking.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,21 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("7e1865bf3b383ce7e1b1");
-        accessTokenDTO.setClient_secret("74143192fef6494d122f8db64d470b20943b7969");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
