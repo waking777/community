bd4458fe752c288374dbb2b61ac1bc7eb758e902
waking777 <756274132@qq.com>
2019-09-10 11:18:45 +0800

    将数据库有h2换成了mysql

--- src/main/java/life/waking/community/mapper/UserMapper.java
+++ src/main/java/life/waking/community/mapper/UserMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-    @Insert("insert into user (name,account_id,token.gmt_create,gmt_modified) values (#{name},#{accountId},#{token}," +
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token}," +
             "#{gmtCreate},#{gmtModified})")
     void insert(User user);
 }
