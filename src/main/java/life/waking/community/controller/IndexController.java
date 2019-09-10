d44662d193af3819ce241626f83d0445bef982e1
waking777 <756274132@qq.com>
2019-09-10 16:46:19 +0800

    写cookie和session

--- src/main/java/life/waking/community/controller/IndexController.java
+++ src/main/java/life/waking/community/controller/IndexController.java
@@ -1,14 +1,37 @@
 package life.waking.community.controller;
 
+import life.waking.community.mapper.UserMapper;
+import life.waking.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if ("token".equals(cookie.getName())){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null){
+
+                }
+                break;
+            }
+        }
+
         return "index";
     }
 }
--- src/main/java/life/waking/community/mapper/UserMapper.java
+++ src/main/java/life/waking/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package life.waking.community.mapper;
 import life.waking.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 
 @Mapper
@@ -10,4 +12,7 @@ public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token}," +
             "#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
