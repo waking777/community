28cb3764280f77454fa174bbd64d9e2e16ea6c69
waking777 <756274132@qq.com>
2019-09-12 16:05:05 +0800

    实现发布判断

--- src/main/java/life/waking/community/controller/PublishController.java
+++ src/main/java/life/waking/community/controller/PublishController.java
@@ -1,12 +1,68 @@
 package life.waking.community.controller;
 
+import life.waking.community.mapper.QuestionMapper;
+import life.waking.community.mapper.UserMapper;
+import life.waking.community.model.Question;
+import life.waking.community.model.User;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/publish")
     public String pulish() {
         return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("tag") String tag,
+            HttpServletRequest request,
+            Model model) {
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if ("token".equals(cookie.getName())) {
+                String token = cookie.getValue();
+                user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
+            return "publish";
+        }
+
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
+
+
+        questionMapper.create(question);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/life/waking/community/mapper/QuestionMapper.java
@@ -0,0 +1,13 @@
+package life.waking.community.mapper;
+
+import life.waking.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+
+    @Insert("insert into (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description}," +
+            "#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    void create(Question question);
+}
